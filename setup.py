7e74c175052aee2ed5f61b1f16cdf4d0201841c5
theAbdoSabbagh <abdulghani.alaa.sabbagh@gmail.com>
2024-05-24 23:49:57 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+requirements = []
+
+readme = ""
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="BetterThreads",
+    author="theAbdoSabbagh",
+    url="https://github.com/theAbdoSabbagh/BetterThreads",
+    project_urls={
+        "Documentation": "https://github.com/theAbdoSabbagh/BetterThreads/blob/main/README.md",
+        "Issue tracker": "https://github.com/theAbdoSabbagh/BetterThreads/issues",
+    },
+    version="0.0.1",
+    packages=["BetterThreads"],
+    license="MIT",
+    description="Threads, but with more features!",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.8.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+)
